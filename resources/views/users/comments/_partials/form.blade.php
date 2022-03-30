c323b6f64abb6dd0ba696ce9f7b1e1d23f29e442
hudsonviana <hudson1206@gmail.com>
2022-03-30 00:21:08 -0300

    Comment store

---
+++ resources/views/users/comments/_partials/form.blade.php
@@ -0,0 +1,27 @@
+<div class="w-full bg-white shadow-md rounded px-8 py-12">
+    @csrf
+    <textarea name="body" cols="30" rows="10" placeholder="Comentário" class="form-control
+    block
+    w-full
+    px-3
+    py-1.5
+    text-base
+    font-normal
+    text-gray-700
+    bg-white bg-clip-padding
+    border border-solid border-gray-300
+    rounded
+    transition
+    ease-in-out
+    m-0 
+    focus:text-gray-700 focus:bg-white focus:border-blue-300
+    "></textarea>
+    
+    <label for="visible">
+        <input type="checkbox" name="visible" id="visible">
+        Visível?
+    </label>
+
+    <button type="submit" class="w-full shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">Salvar</button>
+    <a href="{{ route('users.index') }}" class="bg-green-500 rounded-full text-white px-4 my-8 text-sm">Voltar</a>
+</div>
--- resources/views/users/comments/index.blade.php
+++ resources/views/users/comments/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <h1 class="text-2x1 font-semibold leading-tigh py-2">
     Comentários do usuário {{ $user->name }}
-    <a href="{{ route('users.create') }}" class="bg-blue-900 rounded-full text-white px-4 text-sm">+</a>
+    <a href="{{ route('comments.create', $user->id) }}" class="bg-blue-900 rounded-full text-white px-4 text-sm">+</a>
 </h1>
 
 <form action="{{ route('users.index') }}" method="get" class="py-5">
@@ -26,9 +26,6 @@
             <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                 Editar
             </th>
-            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                Detalhes
-            </th>
         </tr>
     </thead>
     <tbody>
@@ -38,13 +35,10 @@
                 {{ $comment->body }}
             </td>
             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                {{ $comment->visible }}
+                {{ $comment->visible ? 'SIM' : 'NÃO' }}
             </td>
             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                 <a href="{{ route('users.edit', $user->id) }}" class="bg-green-200 hover:bg-green-300 rounded-full py-2 px-6">Editar</a>
-            </td>
-            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                <a href="{{ route('users.show', $user->id) }}" class="bg-orange-200 hover:bg-orange-300 rounded-full py-2 px-6">Detalhes</a>
             </td>
         </tr>
         @endforeach
